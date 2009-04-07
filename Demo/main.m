897a0f7df95b9e713e6c5e670dc29fc2b89f8b89
Kevin Ballard <kevin@sb.org>
2009-04-06 22:50:01 -0700

    Replace the Demo project with a command-line tool

--- Demo/main.m
+++ Demo/main.m
@@ -1,14 +1,34 @@
-//
-//  main.m
-//  FeedParser
-//
-//  Created by Kevin Ballard on 4/3/09.
-//  Copyright Kevin Ballard 2009 . All rights reserved.
-//
+/*
+ *  main.m
+ *  FeedParser
+ *
+ *  Created by Kevin Ballard on 4/6/09.
+ *  Copyright 2009 Kevin Ballard. All rights reserved.
+ *
+ */
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
+#import "FeedParser.h"
 
-int main(int argc, char *argv[])
-{
-    return NSApplicationMain(argc, (const char **) argv);
+int main() {
+	NSAutoreleasePool *pool = [NSAutoreleasePool new];
+	// this sample RSS is just the first few fields of the sample-rss-2.rss file
+	NSString *rss = @"\
+<?xml version=\"1.0\"?>\
+<rss version=\"2.0\">\
+   <channel>\
+      <title>Liftoff News</title>\
+      <link>http://liftoff.msfc.nasa.gov/</link>\
+      <description>Liftoff to Space Exploration.</description>\
+	</channel>\
+</rssd>";
+	NSData *data = [rss dataUsingEncoding:NSUTF8StringEncoding];
+	NSError *error;
+	FPFeed *feed = [FPParser parsedFeedWithData:data error:&error];
+	if (feed) {
+		printf("feed: %s", [[feed description] UTF8String]);
+	} else {
+		printf("error: %s", [[error localizedDescription] UTF8String]);
+	}
+	[pool release];
 }
